da9373c77d487a6021a8edfede503db1581f2cd6
Iliyan Videnov <iliyan.videnov@sap.com>
2016-07-03 22:18:59 +0300

    lecture service added

--- src/main/java/bg/uni/sofia/fmi/homeworksystem/rest/MyApplication.java
+++ src/main/java/bg/uni/sofia/fmi/homeworksystem/rest/MyApplication.java
@@ -9,6 +9,7 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import bg.uni.sofia.fmi.homeworksystem.services.CourseService;
 import bg.uni.sofia.fmi.homeworksystem.services.HomeService;
+import bg.uni.sofia.fmi.homeworksystem.services.LectureService;
 import bg.uni.sofia.fmi.homeworksystem.services.SubmissionService;
 import bg.uni.sofia.fmi.homeworksystem.services.TraineeService;
 import bg.uni.sofia.fmi.homeworksystem.services.TrainerService;
@@ -20,6 +21,7 @@ public class MyApplication extends ResourceConfig {
 	   register(TraineeService.class);
 	   register(SubmissionService.class);
 	   register(TrainerService.class);
+	   register(LectureService.class);
 	   register(MOXyJsonProvider.class);
 	   register(JsonProcessingFeature.class);
 	   packages("org.glassfish.jersey.examples.jsonp.resource");
---
+++ src/main/java/bg/uni/sofia/fmi/homeworksystem/services/LectureService.java
@@ -0,0 +1,78 @@
+package bg.uni.sofia.fmi.homeworksystem.services;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import bg.uni.sofia.fmi.homeworksystem.dao.CourseDAO;
+import bg.uni.sofia.fmi.homeworksystem.dao.LectureDAO;
+import bg.uni.sofia.fmi.homeworksystem.model.Course;
+import bg.uni.sofia.fmi.homeworksystem.model.Lecture;
+
+@Path("hmwsrest/v1/course")
+public class LectureService {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(SubmissionService.class);
+	private final String ERROR_PARSING_DATE_MESSAGE = "Error accured while date is being parsed to specific format.";
+	
+	@Inject
+	private CourseDAO courseDAO;
+	
+	@Inject
+	private LectureDAO lectureDAO;
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{courseId}/lecture")
+	public Response getLecturesByCourseId(@PathParam("courseId") String courseIdStr) {
+		long courseId = Long.parseLong(courseIdStr);
+		
+		Course course = courseDAO.getById(Course.class, courseId);
+		
+		JsonArray jArray = new JsonArray();
+		for (Lecture c : course.getLectures()) {
+			jArray.add(c.toJson());
+		}
+		
+		return Response.ok(jArray.toString()).build();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/lecture")
+	public Response addLecture(String data) {
+		JsonObject userData = new JsonParser().parse(data).getAsJsonObject();
+		long courseId = Long.parseLong(userData.get("courseId").getAsString());
+		String task = userData.get("task").getAsString();
+		String endDateStr = userData.get("endDate").getAsString();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("DD-MMM-YYYY HH:mm");
+		Date endDate = null;
+		try {
+			endDate = dateFormat.parse(endDateStr);
+		} catch (ParseException e) {
+			LOGGER.error(ERROR_PARSING_DATE_MESSAGE ,e);
+		}
+		
+		Course course = courseDAO.getById(Course.class, courseId);
+		Lecture newLecture = new Lecture(task, endDate, course);
+		lectureDAO.save(newLecture);
+		return Response.ok().build();
+	}
+}
